a8189b60776194520c7a8b834e5319d4f52a3410
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-12 03:16:59 -0800

    Create c702.lua

---
+++ c702.lua
@@ -0,0 +1,35 @@
+--Acquired Immunodeficiency Syndrome
+
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0x1b) --ATP Counters
+	c:EnableCounterPermit(0x8) --Distress Counters
+	c:EnableCounterPermit(0x7) --Delirium Counters
+	c:SetCounterLimit(0x1b,6) --ATP Counters Limit
+	aux.AddFieldSkillProcedure(c,2,false)
+
+	local e0=Effect.CreateEffect(c)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_STARTUP)
+	e0:SetRange(0xff)
+	e0:SetOperation(s.stop)
+	c:RegisterEffect(e0)
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+
+end
+
+function s.stfilter(c)
+	return c:IsCode(1)
+end
+
+function s.stop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.stfilter,tp,0xff,0,c)
+	if #g>0 and Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+	end
+end
