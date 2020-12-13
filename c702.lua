09ec19ee8a736750491020d524bb27ce2af50e4a
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-13 04:22:42 -0800

    Update c702.lua

--- c702.lua
+++ c702.lua
@@ -33,13 +33,13 @@ function s.initial_effect(c)
 	e3:SetTargetRange(LOCATION_MZONE,0)
 	c:RegisterEffect(e3)
 
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_DISABLE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetTargetRange(0,LOCATION_MZONE)
-	e4:SetTarget(s.Target)
-	c:RegisterEffect(e4)
+	--local e4=Effect.CreateEffect(c)
+	--e4:SetType(EFFECT_TYPE_FIELD)
+	--e4:SetCode(EFFECT_DISABLE)
+	--e4:SetRange(LOCATION_MZONE)
+	--e4:SetTargetRange(0,LOCATION_MZONE)
+	--e4:SetTarget(s.Target)
+	--c:RegisterEffect(e4)
 
 end
 
@@ -62,6 +62,6 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCounter(0,1,1,0x4)>=5
 end
 
-function s.Target(e,tp,eg,ep,ev,re,r,rp,chk)
-	return c:IsCode(604,605,606)
-end
+--function s.Target(e,tp,eg,ep,ev,re,r,rp,chk)
+	--return c:IsCode(604,605,606)
+--end
