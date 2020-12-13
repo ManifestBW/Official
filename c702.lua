58bb48edb8699e3b327c66d1f0f8945b44dfc53b
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-13 05:07:57 -0800

    Update c702.lua

--- c702.lua
+++ c702.lua
@@ -18,7 +18,6 @@ function s.initial_effect(c)
 	e2:SetCategory(CATEGORY_COUNTER)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCountLimit(1)
 	e2:SetCode(EVENT_PHASE+PHASE_DRAW)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCondition(s.con1)
@@ -52,9 +51,9 @@ function s.stfilter(c)
 end
 
 function s.op1(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.stfilter,tp,LOCATION_HAND+LOCATION_DECK,0,0)
+	local g=Duel.GetMatchingGroup(s.stfilter,tp,LOCATION_HAND+LOCATION_DECK,0,c)
 	Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-	local g2=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsCanAddCounter,0x4,12),1-tp,LOCATION_FZONE,0,0)
+	local g2=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsCanAddCounter,0x4,12),1-tp,LOCATION_FZONE,0,c)
 	g2:ForEach(Card.AddCounter,0x4,12)
 end
 
