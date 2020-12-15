18f716ee28073a83f80193f9d736111968e6926d
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-15 12:43:15 -0800

    Update 812.lua

--- 812.lua
+++ 812.lua
@@ -3,14 +3,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Pendulum.AddProcedure(c)
-
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCode(EFFECT_ADD_TYPE)
-	e1:SetLocation(LOCATION_HAND+LOCATION_FIELD)
-	e1:SetValue(TYPE_PENDULUM)
-	c:RegisterEffect(e1)
+	
 
 end
