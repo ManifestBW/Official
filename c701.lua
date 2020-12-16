06e8838f002e3a1e5319e56e4fce703528628991
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-16 11:40:17 -0800

    Update c701.lua

--- c701.lua
+++ c701.lua
@@ -160,6 +160,7 @@ function s.cfop(e,tp,eg,ep,ev,re,r,rp)
 	local code=tc:GetOriginalCode()
 	Duel.Hint(HINT_MESSAGE,tp,aux.Stringid(code,0))
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(code,0))
+	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(code,0))
 	e:SetLabel(op)
 	end
 end
