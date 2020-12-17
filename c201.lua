dd555a64ba00042569acac66f26afb5f4da76de6
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-17 13:53:26 -0800

    Update c201.lua

--- c201.lua
+++ c201.lua
@@ -385,7 +385,7 @@ end
 function s.CrackleOperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	local ctg=c:GetCounter(0x8)
-	c:RemoveCounter(0x8,ctg)
-	tc:AddCounter(0x8,ctg)
+	local ct=c:GetCounter(0x8)
+	c:RemoveCounter(0x8,ct)
+	tc:AddCounter(0x8,ct)
 end
