f890a7a75226543b0f7e0bbc26ffd534c05e3f46
ManifestBW <75697756+ManifestBW@users.noreply.github.com>
2020-12-18 22:07:10 -0800

    Update c501.lua

--- c501.lua
+++ c501.lua
@@ -130,15 +130,15 @@ function s.PathogenFilter(c,tp)
 end
 
 function s.ManifestTarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.eqfilter(chkc,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.PathogenFilter,tp,LOCATION_SZONE,0,1,nil,tp)
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	local ct=math.min((Duel.GetLocationCount(tp,LOCATION_SZONE)),1)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	--if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.eqfilter(chkc,tp) end
+	--if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		--and Duel.IsExistingTarget(s.PathogenFilter,tp,LOCATION_SZONE,0,1,nil,tp)
+		--and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	--local ct=math.min((Duel.GetLocationCount(tp,LOCATION_SZONE)),1)
+	--Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local sg=Duel.GetMatchingGroup(s.PathogenFilter,tp,LOCATION_SZONE,0,1,1,nil,tp)
-	--local g=sg:RandomSelect(tp,1)
-	Duel.SetTargetCard(sg)
+	local g=sg:RandomSelect(tp,1)
+	Duel.SetTargetCard(g)
 end
 
 function s.ManifestOperation(e,tp,eg,ep,ev,re,r,rp)
